33f0e4853bf63beadca7a29eca6513184411844f
Kakaranish <stasiekgruz@gmail.com>
2018-04-25 20:27:48 +0200

    Rewrited main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <conio.h>
+
+#include "Stack.h"
+#include "Stack.cpp"
+#include "Queue.h"
+#include "Queue.cpp"
+
+
+int main(int argc, char* argv[]){
+
+
+
+	_getch();
+	return 0;
+}
